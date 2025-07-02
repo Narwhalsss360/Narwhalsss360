f2e45acc68bb6736ed8526786d3900987780a655
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-07-02 18:43:26 -0400

    Added check for linux *first* then set shell as fish exists

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -14,20 +14,20 @@ vim.g.laztvim_picker = "telescope"
 vim.g.zig_fmt_autosave = false
 
 
-if vim.fn.has("win32") then
+if vim.fn.has("linux") then
+    if vim.fn.executable("fish") then
+        vim.o.shell = "fish"
+    end
+elseif vim.fn.has("win32") then
     if vim.fn.executable("pwsh") == 1 then
-    vim.o.shell = "pwsh"
+        vim.o.shell = "pwsh"
     else
-    vim.o.shell = "powershell"
+        vim.o.shell = "powershell"
     end
 
-    vim.o.shellcmdflag =
-    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-
+    vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
     vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
     vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
     vim.o.shellquote = ""
     vim.o.shellxquote = ""
 end
