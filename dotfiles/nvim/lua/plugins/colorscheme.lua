3a6f5035dcd56b2ddb48463ca5378ba3b2be4c70
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-20 13:35:03 -0400

    Added colorscheme switching bsaed off of time of day

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,20 @@
+function colorschemeForTime()
+    local hour = tonumber(os.date("%H"))
+
+    if 10 <= hour and hour <= 17 then
+      return "evening"
+    elseif 18 <= hour and hour <= 22 then
+      return "slate"
+    else
+      return "murphy"
+    end
+end
+
 return {
-  "LazyVIm/LazyVim",
-  opts = {
-    colorscheme = "wildcharm",
-  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = colorschemeForTime(),
+    },
+  }
 }
